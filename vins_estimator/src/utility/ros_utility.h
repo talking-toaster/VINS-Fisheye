5ea1cb8d72e76295b71bf4b66153916e228ca1f0
talkingtoaster <xz.xuezhen@gmail.com>
2023-11-20 19:36:51 +0800

    添加cv_bridge，适配opencv-gpu

--- vins_estimator/src/utility/ros_utility.h
+++ vins_estimator/src/utility/ros_utility.h
@@ -2,6 +2,7 @@
 #include <geometry_msgs/Pose.h>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
+#include <utility/ros_cv_bridge.hpp>
 
 geometry_msgs::Pose pose_from_PQ(Eigen::Vector3d P, 
     const Eigen::Quaterniond & Q);
